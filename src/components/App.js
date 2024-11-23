073663b22da34e2f3dba97bd0364b3a0e8502868
swetha-A130 <swethachintalapudi6@gmail.com>
2024-11-23 23:31:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1>Hi its done</h1>
+    <p>I am learning React. My life is getting better.</p></div>
   )
 }
 
